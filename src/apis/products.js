b66fa766784eef1ba34cb6cd0e3dda0efbb9ac98
Alsha Behera <alshabehera7@gmail.com>
2024-08-05 23:23:07 +0530

    Implemented search functionality to product listing page

--- src/apis/products.js
+++ src/apis/products.js
@@ -2,6 +2,7 @@ import axios from "axios";
 // this is to show a single product details
 const show = (slug) => axios.get(`products/${slug}`);
 //this is to fetch data of all the products
-const fetch = () => axios.get("products");
+const fetch = (params) => axios.get("products", { params });
+
 const productsApi = { show, fetch };
 export default productsApi;
