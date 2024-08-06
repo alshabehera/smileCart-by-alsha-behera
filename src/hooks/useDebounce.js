b0dd58b6bd006ab82a68922b4ace1b3aea2eda7e
Alsha Behera <alshabehera7@gmail.com>
2024-08-06 21:55:25 +0530

    Created custom hook for debouncing

--- src/hooks/useDebounce.js
+++ src/hooks/useDebounce.js
@@ -8,6 +8,7 @@ const useDebounce = (value) => {
       setDebouncedValue(value);
     }, 350);
 
+    // it will call the cleanup function for the previous render before re-running
     return () => {
       clearTimeout(timerId);
     };
